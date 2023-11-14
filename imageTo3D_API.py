76d99a20b194f6ef65457b94cd8732192a0367d3
Konrad <konrados@stud.ntnu.no>
2023-11-14 16:01:42 +0100

    Finished plane creation, mesh next

--- imageTo3D_API.py
+++ imageTo3D_API.py
@@ -6,16 +6,20 @@ from tkinter import filedialog
 
 
 class ImageTo3D:
-    def __init__(self, image_path, reduction_factor=10, extrudeScale=5):
+    def __init__(self, image_path=None, reduction_factor=10, extrudeScale=5):
         if reduction_factor is None:
             reduction_factor = self.reccomendedReduction()
         self.image_path = image_path
+        if image_path is None:
+            self.upload_file()
         self.reduction_factor = reduction_factor
         self.extrudeScale = extrudeScale/25
         # Open the image and convert to black and white
         self.image = Image.open(self.image_path)
         self.image.convert("RGB")
         self.image_bw = self.image.convert('L')
+
+        self.mesh=None
 
     def generate_mesh(self):
         image = self.image
@@ -37,33 +41,6 @@ class ImageTo3D:
             points = np.column_stack((x, y, z*(self.extrudeScale)))
         points = np.column_stack((x*5, y*5, z*(self.extrudeScale))) 
 
-        with open("points.txt", "w") as f:
-            for point in points:
-                f.write(f"{point[0]} {point[1]} {point[2]}\n")
-
-        #preview the points
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlim([0, image_bw.width*5])
-        ax.set_ylim([0, image_bw.height*5])
-        ax.set_zlim([0, 255])
-        ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='b', marker='.')
-
-        # Set axis labels
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-
-        # Show the plot
-        plt.show()
-
-        face_count = (image_bw.width-1) * (image_bw.height-1) * 2
-        
-        faces = np.zeros((face_count, 3), dtype=int)
-        
-        #TODO make an algorithm that makes the faces based on the points
-        #husk, for å finne trekanten er det punkt i, i+1, i+width
-
         faces = []
         for i in range(image_bw.height - 1):
             for j in range(image_bw.width - 1):
@@ -75,22 +52,16 @@ class ImageTo3D:
 
         faces = np.array(faces)
         mesh = meshio.Mesh(points=points, cells=[("triangle", faces)])
+        self.mesh=mesh
         return mesh
 
-        # # Create a mesh using meshio
-        # mesh = meshio.Mesh(points=vertices, cells=[("triangle", np.array(faces))])
 
-        # # Save the mesh as an STL file
-        # meshio.write("cube.stl", mesh)
-
-
-
-
-
-
-
-    def export_mesh(self, filename, mesh):
-        meshio.write("output.stl", mesh)
+    def export_mesh(self, mesh):
+        f_types = [('STL Files', '*.stl')]
+        filename = filedialog.asksaveasfilename(filetypes=f_types)
+        if not filename.endswith(".stl"):
+            filename+=".stl"
+        meshio.write(filename, mesh)
 
         print(f"3D model exported to {filename}")
     
@@ -99,29 +70,20 @@ class ImageTo3D:
         height = self.image.height
         return 1000/(width*height)
 
-def upload_file():
-    #lets the user upload an image, and returns the path to the image
-    f_types = [('PNG Files', '*.png'),('JPG Files', '*.jpg')]
-    filename = filedialog.askopenfilename(filetypes=f_types)
-    return filename
-def savefile():
-    #lets the user save the file
-    f_types = [('STL Files', '*.stl')]
-    filename = filedialog.asksaveasfilename(filetypes=f_types)
-    return filename
+    def upload_file(self):
+        #lets the user upload an image, and returns the path to the image
+        f_types = [('JPG Files', '*.jpg')]
+        self.image_path = filedialog.askopenfilename(filetypes=f_types)
 
-image = Image.open("C:\Bilder\Cursed\Screenshot_20221215-015400_Instagram.jpg")
+    def preview(self):
+        #shows a preview of the image
+        plt.imshow(self.image)
+        plt.show()
 
 if __name__ == "__main__":
-    # path="C:\Bilder\Cursed\sample_a033837ed4d8eee931e9a725dbd07c34.jpg"
-    # path="ikon.png"
-    #path="C:\Bilder\Cursed\RDT_20210820_0810126969680436670274982.jpg"
-    path="C:\Bilder\Cursed\Screenshot_20230409-234115_Instagram.jpg"
-    path=upload_file()
-    imageTo3D = ImageTo3D(path, reduction_factor=10,extrudeScale=10)
+    imageTo3D = ImageTo3D(reduction_factor=10,extrudeScale=10)
     mesh_data = imageTo3D.generate_mesh()
-    filename = savefile()
-    imageTo3D.export_mesh(filename, mesh_data)
+    imageTo3D.export_mesh(mesh_data)
 
 
 
